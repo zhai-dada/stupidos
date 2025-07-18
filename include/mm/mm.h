d3b3564651f135914a95bab2edd84c7c12159327
zhai-dada <zhaipengwei0728@gmail.com>
2025-07-18 13:24:32 +0800

    测试memzero
    Signed-off-by: zhai-dada <zhaipengwei0728@gmail.com>

--- include/mm/mm.h
+++ include/mm/mm.h
@@ -1,4 +1,8 @@
 #ifndef	_MM_H
 #define	_MM_H
 
+#include <stdint.h>
+
+void memzero(void* addr, u64 count);
+
 #endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,13 @@
-int kernel_main(void)
+#include <mm/mm.h>
+
+s32 kernel_main(void)
 {
+    u64 a = 0xffffffffffffffff;
+    memzero(&a, sizeof(s32));
     while(1)
     {
-        ;
+        a++;
+        a++;
     }
     return 0;
 }
